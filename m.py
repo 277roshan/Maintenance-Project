5028079f9f109c31a478b37c5953631e33ddb054
Roshan Thapaliya <roshan.thapaliya@bison.howard.edu>
2015-11-23 19:29:15 -0500

    Function to create required data structure

--- m.py
+++ m.py
@@ -1,17 +1,23 @@
 
 
-def data_structure():
+def data_structure(length_of_module):
 	tree = {}
 	with open('input.txt') as f:
 		while True:
 			a = f.readline()
 			a = a.split()
-			if a[0] == '*':
+			if len(a[0]) > length_of_module or len(a[1]) > length_of_module:
+				return False
+			if a[0] == '**':
 				break
-			tree[a[0]]=[a[1]]
-			
+			if a[0] not in tree.keys():
+				tree[a[0]]=[a[1]]
+			else:
+				tree[a[0]].append(a[1])
+
 	return tree
 
 
-print data_structure()
+print data_structure(3)
+
 
